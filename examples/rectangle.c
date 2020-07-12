0c157124dd4f75d21cf0a969b3df93c2a201a491
PhurinatC <phurinat.chu@mail.pbru.ac.th>
2020-07-12 22:43:11 +0700

    add rectangle

---
+++ examples/rectangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+#include <conio.h> 
+int main(void) 
+    {  
+        int     width, height;  
+        float area;  
+        printf("Input width : ");  
+        scanf("%d", &width);  
+        printf("Input height : ");  
+        scanf("%d", &height);  
+        area = width * height;  
+        printf("\nArea of rectangle : %.2f\n",area);  
+        printf("Have a nice day...\n");  
+        getch();  
+        return 0; 
+    }
