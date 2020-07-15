f09e0695a486ef9640b2aa2c50551afea40de856
PhurinatC <phurinat.chu@mail.pbru.ac.th>
2020-07-15 09:48:57 +0700

    edit/add works 15.07

---
+++ Works/105ass2July15.c
@@ -0,0 +1,30 @@
+#include <stdio.h> 
+#include <conio.h> 
+int  main() 
+{  
+    int  num1 = 75;  //num1  คือชื่อตัวแปร  
+    int  num2 = 12;  //num2  คือชื่อตัวแปร
+    float  fnum1 = 75;  //fnum1 คือชื่อตัวแปร
+ 
+    printf("Add    : %d+%d  = %d\n",num1,num2,num1+num2);   //ให้แสดงคือใน %d ตามลำดับของสูตรตัวแปรที่กำหนด
+    printf("Subtract   : %d-%d  = %d\n",num1,num2,num1-num2);  
+    printf("Multiply   : %d*%d  = %d\n",num1,num2,num1*num2);  
+    printf("Devide (int)  : %d/%d  = %d\n",num1,num2,num1/num2);  
+    printf("Devide (float)   : %.2f/%d  = %.2f\n\n\n",fnum1,num2,fnum1/num2);
+
+    int  result1;  
+    float  result2;  
+    result1 = num1+num2;    //ทำการกำหนดค่าตัวแปรโดยใช้สมการ ก่อนที่จะเริ่มคำสั่งในบรรทัดต่อไป 
+    printf("Add    : %d+%d  = %d\n",num1,num2,result1);  
+    result1 = num1-num2;  
+    printf("Subtract   : %d-%d  = %d\n",num1,num2,result1);  
+    result1 = num1*num2;  
+    printf("Multiply   : %d*%d  = %d\n",num1,num2,result1);  
+    result1 = num1/num2;  
+    printf("Devide (int)  : %d/%d  = %d\n",num1,num2,result1);  
+    result2 = fnum1/num2;  
+    printf("Devide (float)  : %.2f/%d  = %.2f\n",fnum1,num2,result2);    
+
+    getch();   
+    return 0;    
+}
