265171780521697436882d6e5ffee674566e362e
PhurinatC <phurinat.chu@mail.pbru.ac.th>
2020-07-15 09:14:12 +0700

    add myname105_function

---
+++ Works/myname105_function.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void myname()
+{
+    printf("Student ID :634244105 \n"); 
+    printf("Full Name :Phurinat Chuaiyim \n");
+    printf("Nickname :New \n");
+    printf("Year :2563/1 \n");
+    printf("Computer Science \n");
+    printf("Information Technology \n");
+    printf("Phetchaburi Rajabhat University \n");
+}
+
+int main() 
+{
+    myname();
+    getchar();
+    return 0;
+}
