5109352262af5a8d359bfa675fb6626b27d65d87
PhurinatC <phurinat.chu@mail.pbru.ac.th>
2020-07-17 18:29:42 +0700

    add sum_average

---
+++ examples/sumaverage.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ 
+int main()
+{
+  int n, sum = 0, c, value, a;
+ 
+  printf("How many numbers you want to add?\n");
+  scanf("%d", &n);
+ 
+  printf("Enter %d integers\n", n);
+ 
+  for (c = 1; c <= n; c++)
+  {
+    scanf("%d", &value);
+    sum = sum + value;
+  }
+ 
+  printf("Sum of the integers = %d\n", sum);
+  
+  a = sum/n;
+  
+  printf("The average of the sum = %d ",a);
+
+  return 0;
+  
+}
