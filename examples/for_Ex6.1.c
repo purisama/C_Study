5d438335e35d59233fe24e9c26c5319202899579
PhurinatC <phurinat.chu@mail.pbru.ac.th>
2020-07-19 20:14:36 +0700

    edit Ex6.1,Ex6.2

--- examples/for_Ex6.1.c
+++ examples/for_Ex6.1.c
@@ -5,8 +5,8 @@ int i;
 
 int main()
 {
-    for(i = 1; i<=12; i++)
-        printf(" 2 x %d = %d \n", i , 2*i);
+    for(i = 1; i < 5; i++)
+        printf("Number %d\n", i);
     getch();
     return 0;
 }
