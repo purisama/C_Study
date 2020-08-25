d9934adf0a9e4c79df3ead6db227e348fa3e9d26
PhurinatC <phurinat.chu@mail.pbru.ac.th>
2020-08-25 22:46:22 +0700

    moreFunc WIP

---
+++ Works/105no1moreFunc.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+
+float getnum()
+{
+    printf("Enter the number : ");
+    scanf("%f",&num);
+}
+
+float getprice()
+{
+    printf("\nEnter the price : ");
+    scanf("%f",&price);
+}
+
+float costprocess(float n1,float n2)
+{
+    float cost;
+    cost = n1*n2;
+    return cost;
+}
+
+int main ()
+{
+    float num,price;
+    float cost;
+
+    cost = costprocess(num, price);
+    printf("\nTotal cost : %.2f ",cost);
+    getch();
+    return 0;
+}
