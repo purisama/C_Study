1dcb482f25146b780a661c2f5fc1db1296d02661
PhurinatC <phurinat.chu@mail.pbru.ac.th>
2020-09-23 04:03:05 +0700

    fix work decision7

--- Works/progDecision/105progDecision7.c
+++ Works/progDecision/105progDecision7.c
@@ -13,15 +13,15 @@ int main()
     }
     else if (ts >= 1000 && ts < 2000)
     {
-        printf("\n%.sf",ts+(ts*0.05));
+        printf("\n%.2f",ts+(ts*0.05));
     }
     else if (ts >= 2000 && ts < 5000)
     {
-        printf("\n%.sf",ts+(ts*0.10));
+        printf("\n%.2f",ts+(ts*0.10));
     }
     else if (ts >= 5000 && ts < 10000)
     {
-        printf("\n%.sf",ts+(ts*0.15));
+        printf("\n%.2f",ts+(ts*0.15));
     }
     else if (ts > 10000 )
     {
